cc9fc77127ad1a329e7814e9cccfd6708baaa7f5
Andrew Molyuk <andrew@molyuk.com>
2024-03-19 10:41:27 +0200

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MantineProvider, Text } from '@mantine/core';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import pkg from '../package.json';
 
 import '@mantine/core/styles.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Text>Hello checkmeup.net! Version: {pkg.version}</Text>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider>
-      <Text>Hello checkmeup.net! Version: {pkg.version}</Text>
+      <RouterProvider router={router} />
     </MantineProvider>
   </React.StrictMode>,
 );

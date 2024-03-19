5e66c9c5ff3512a13f1aae638071aa8f1feeed01
Andrew Molyuk <andrew@molyuk.com>
2024-03-19 09:32:38 +0200

    Remove unnecessary App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function App() {
-  return <>Hello checkmeup.net!</>;
-}
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode>Hello checkmeup.net!</React.StrictMode>);

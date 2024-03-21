f0617e68657dd158eac1c8456d389336bc017a16
Andrew Molyuk <andrew@molyuk.com>
2024-03-21 21:39:24 +0200

    Fix styles

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -1,34 +1,11 @@
-import { useState } from 'react';
-import { Container, Anchor, Group, Burger, Box, Text } from '@mantine/core';
+import { Container, Group, Burger, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { Logo } from '@/widgets/logo';
-import classes from './header.module.css';
 
-const mainLinks = [
-  { link: '/', label: 'Home' },
-  { link: '/blog', label: 'Blog' },
-  { link: '/stats', label: 'Stats' },
-  { link: '/about', label: 'About' },
-];
+import classes from './styles.module.css';
 
 export const Header = () => {
   const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(0);
-
-  const mainItems = mainLinks.map((item, index) => (
-    <Anchor<'a'>
-      href={item.link}
-      key={item.label}
-      className={classes.mainLink}
-      data-active={index === active || undefined}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(index);
-      }}
-    >
-      {item.label}
-    </Anchor>
-  ));
 
   return (
     <header className={classes.header}>
@@ -37,11 +14,6 @@ export const Header = () => {
           <Logo />
           <Text span>checkmeup.net</Text>
         </Group>
-        <Box className={classes.links} visibleFrom="sm">
-          <Group gap={0} justify="flex-end" className={classes.mainLinks}>
-            {mainItems}
-          </Group>
-        </Box>
         <Burger opened={opened} onClick={toggle} className={classes.burger} size="sm" hiddenFrom="sm" />
       </Container>
     </header>

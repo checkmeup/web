52585f0c5b0f6594c486d10e5ebabc5aa84d5d28
Andrew Molyuk <andrew@molyuk.com>
2024-03-23 21:45:29 +0200

    docs: add comments about commmit structure and types

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,1 +1,13 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+// https://www.conventionalcommits.org/en/v1.0.0/
+
+// The commit message should be structured as follows:
+
+// <type>[optional scope]: <description>
+
+// <type> could be fix:, feat:, build:, chore:, ci:, docs:, style:, refactor:, perf:, test:, and others
+// <scope> is optional and could be anything specifying the place of the commit change
+// <description> should be a short description of the change
+
+// A commit that appends a ! after the type/scope, introduces a breaking API change
+
+https: module.exports = { extends: ['@commitlint/config-conventional'] };

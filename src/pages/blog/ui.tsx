956a7ba6923f59f97560b554a951856a038eef84
Andrew Molyuk <andrew@molyuk.com>
2024-03-24 11:41:45 +0200

    refactor(stats): arrange texts and clean blog

--- src/pages/blog/ui.tsx
+++ src/pages/blog/ui.tsx
@@ -35,44 +35,6 @@ export const BlogPage = () => {
       <ul>
         <li>As a user I want to receive email notification when a cron job fails so that I can fix it.</li>
       </ul>
-      <h2>Finances and Expenses</h2>
-      <p>
-        The app should be open source and transparent for any aspect of it. It should be able to run on a free tier of
-        monitoring and notifications. For begining I will start to log all my efforts and expenses in the project. It
-        should provide a good overview of the project and the costs of running it for any potential users and
-        contributors.
-      </p>
-      <table width={'100%'}>
-        <thead>
-          <tr>
-            <th align="left">Date</th>
-            <th align="left">Effort</th>
-            <th align="right">Cost</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>19/03/2024</td>
-            <td>Website initial setup and deploy implementation</td>
-            <td align="right">4 hours</td>
-          </tr>
-          <tr>
-            <td>20/03/2024</td>
-            <td>Add layout and adjust code to FSD structure</td>
-            <td align="right">2 hours</td>
-          </tr>
-          <tr>
-            <td>21/03/2024</td>
-            <td>Implement top navigation header and fix minor issues in the pages</td>
-            <td align="right">2 hours</td>
-          </tr>
-          <tr>
-            <td>23/03/2024</td>
-            <td>Move to Netlify deployment and fix active mark on the top navigation</td>
-            <td align="right">3 hours</td>
-          </tr>
-        </tbody>
-      </table>
     </div>
   );
 };

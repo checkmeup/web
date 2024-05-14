701a582ee2bce68f5d091cf11f6d8d8def3fc253
Andrew Molyuk <andrew@molyuk.com>
2024-05-14 21:09:57 +0300

    chore: add google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
-import flowbite from 'flowbite-react/tailwind';
+import flowbite from 'flowbite-react/tailwind'
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
   corePlugins: { preflight: false },
-  theme: { extend: {} },
   plugins: [flowbite.plugin()],
-};
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#eff6ff',
+          100: '#dbeafe',
+          200: '#bfdbfe',
+          300: '#93c5fd',
+          400: '#60a5fa',
+          500: '#3b82f6',
+          600: '#2563eb',
+          700: '#1d4ed8',
+          800: '#1e40af',
+          900: '#1e3a8a',
+          950: '#172554',
+        },
+      },
+    },
+    fontFamily: {
+      body: ['Roboto', 'sans-serif'],
+    },
+  },
+}

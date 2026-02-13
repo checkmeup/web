c0969dcb24b3bb670f269657b92d7b7817196df2
Andrew Molyuk <andrew@molyuk.com>
2026-02-13 21:43:38 +0200

    style: simplify paragraph formatting in App.vue

--- src/app/App.vue
+++ src/app/App.vue
@@ -15,10 +15,7 @@ const counter = useCounterStore()
       <button @click="counter.increment()">Increment</button>
     </section>
 
-    <p>
-      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-      documentation
-    </p>
+    <p>Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the documentation</p>
   </div>
 </template>
 
